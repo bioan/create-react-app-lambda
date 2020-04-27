b976920291c2f472bc1be0264ab1045993006b09
Ioan Botez <85bioan@gmail.com>
2020-04-27 16:38:50 +0200

    Spammy popup

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
           </p>
           <LambdaDemo />
         </header>
-        <PWAPrompt />
+        <PWAPrompt permanentlyHideOnDismiss timesToShow='999' />
       </div>
     )
   }
